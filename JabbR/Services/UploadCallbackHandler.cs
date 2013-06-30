db4b6ce6b8ed9b8720f5ac07668039d953d33103
cmaish <mail@christophermaish.com>
2013-06-29 17:06:26 -0700

    Fix alias vs type for String.format, remove "are we storing messages in database" stuff from message url handling

--- JabbR/Services/UploadCallbackHandler.cs
+++ JabbR/Services/UploadCallbackHandler.cs
@@ -65,7 +65,7 @@ namespace JabbR.Services
             // Notify all clients for the uploaded url
             _hubContext.Clients.Group(roomName).addMessage(messageViewModel, roomName);
 
-            _resourceProcessor.ProcessUrls(new[] { result.Url }, _hubContext.Clients, roomName, message.Id, updateDatabase: true);
+            _resourceProcessor.ProcessUrls(new[] { result.Url }, _hubContext.Clients, roomName, message.Id);
         }
 
         private static string FormatBytes(long bytes)
